d4d197517c91d60534440d1e91c9a918a458721b
kenjiskywalker <git@kenjiskywalker.org>
2014-09-09 22:20:34 +0900

    watchesでnodes見てnode増えたり減ったりしたら hosts_update.rb 実行するようにした

---
+++ node01/hosts_update.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+nodes = JSON.load(STDIN.gets)
+
+hosts_file = "./node01/_hosts"
+
+File.open(hosts_file, "w") do |f|
+  nodes.each do |n|
+    f.puts "#{n['Node']}\t #{n['Address']}"
+  end
+
+  f.close
+end
